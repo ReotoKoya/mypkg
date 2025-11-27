cebebd343fe27703156538a4d314d1735e07bd2a
Reoto Koya <s24c1053jd@s.chibakoudai.jp>
2025-11-28 00:16:05 +0900

    Add a command

---
+++ src/mypkg/mypkg/talker.py
@@ -0,0 +1,20 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query
+
+rclpy.init()
+node = Node("talker")
+
+
+def cb(request, response):
+    if request.name == "小屋玲旺斗":
+        response.age = 20
+    else:
+        response.age = 300
+    return response
+
+
+
+def main():
+    srv =  node.create_service(Query, "query" , cb)
+    rclpy.spin(node)
